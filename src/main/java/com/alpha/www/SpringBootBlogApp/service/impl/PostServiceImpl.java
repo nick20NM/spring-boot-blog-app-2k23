7f601c3095bdf9270a77ab9d0287abcde75cc254
NM <masihnishit@gmail.com>
2023-01-18 17:49:27 +0530

    video 59

--- src/main/java/com/alpha/www/SpringBootBlogApp/service/impl/PostServiceImpl.java
+++ src/main/java/com/alpha/www/SpringBootBlogApp/service/impl/PostServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.alpha.www.SpringBootBlogApp.dto.PostDto;
@@ -44,10 +45,10 @@ public class PostServiceImpl implements PostService {
 	}
 
 	@Override
-	public PostResponse getAllPosts(int pageNo, int pageSize) {
+	public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
 		
 		// create Pageable instance/object
-		Pageable pageable = PageRequest.of(pageNo, pageSize);
+		Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
 		
 		Page<Post> pageObj = postRepository.findAll(pageable);
 		
