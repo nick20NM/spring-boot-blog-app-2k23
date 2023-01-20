0f1fe2f198fe7cfa035dae518f23d165979a9734
NM <masihnishit@gmail.com>
2023-01-20 16:29:07 +0530

    video 91

--- src/main/java/com/alpha/www/SpringBootBlogApp/config/SecurityConfig.java
+++ src/main/java/com/alpha/www/SpringBootBlogApp/config/SecurityConfig.java
@@ -2,18 +2,51 @@ package com.alpha.www.SpringBootBlogApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+	
+	@Bean
+	static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable()
-		.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+		.authorizeHttpRequests(authorize -> 
+//		authorize.anyRequest().authenticated()
+		authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+		.anyRequest().authenticated())
 		.httpBasic(Customizer.withDefaults());
 		return httpSecurity.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails tom = User.builder()
+				.username("tom")
+				.password(passwordEncoder().encode("tom"))
+				.roles("USER")
+				.build();
+		
+		UserDetails admin = User.builder()
+				.username("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(tom, admin);
 	}
 }
--- src/main/java/com/alpha/www/SpringBootBlogApp/controller/PostController.java
+++ src/main/java/com/alpha/www/SpringBootBlogApp/controller/PostController.java
@@ -8,6 +8,7 @@ import static com.alpha.www.SpringBootBlogApp.utils.AppConstants.DEFAULT_SORT_DI
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class PostController {
 	private PostService postService;
 	
 	// create post rest api
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
 		return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -55,6 +57,7 @@ public class PostController {
 	}
 	
 	// update post by id rest api
+	@PreAuthorize("hasRole('ADMIN')")
 	@PutMapping("/{id}")
 	public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable long id){
 		PostDto postDtoResponse = postService.updatePost(postDto, id);
@@ -62,6 +65,7 @@ public class PostController {
 	}
 	
 	// delete post rest api
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deletePost(@PathVariable long id){
 		postService.deletePostById(id);
