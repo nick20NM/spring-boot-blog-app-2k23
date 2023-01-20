de9289698a2637fd257419415ecebf97787b1a6e
NM <masihnishit@gmail.com>
2023-01-20 12:51:44 +0530

    video 89

---
+++ src/main/java/com/alpha/www/SpringBootBlogApp/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.alpha.www.SpringBootBlogApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable()
+		.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+		.httpBasic(Customizer.withDefaults());
+		return httpSecurity.build();
+	}
+}
