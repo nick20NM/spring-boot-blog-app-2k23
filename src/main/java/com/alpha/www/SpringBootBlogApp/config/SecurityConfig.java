8ce110a809eb2a448c9383258d9575a99e2a2a1a
NM <masihnishit@gmail.com>
2023-01-23 23:16:50 +0530

    video 107

--- src/main/java/com/alpha/www/SpringBootBlogApp/config/SecurityConfig.java
+++ src/main/java/com/alpha/www/SpringBootBlogApp/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +17,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.alpha.www.SpringBootBlogApp.security.JwtAuthenticationEntryPoint;
+import com.alpha.www.SpringBootBlogApp.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -23,6 +28,12 @@ public class SecurityConfig {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint authenticationEntryPoint;
+	
+	@Autowired
+	private JwtAuthenticationFilter authenticationFilter;
 	
 	@Bean
 	static PasswordEncoder passwordEncoder() {
@@ -42,7 +53,13 @@ public class SecurityConfig {
 //		authorize.anyRequest().authenticated()
 		authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
 		.requestMatchers("/api/auth/**").permitAll()
-		.anyRequest().authenticated());
+		.anyRequest().authenticated())
+		.exceptionHandling(exception -> 
+		exception.authenticationEntryPoint(authenticationEntryPoint))
+		.sessionManagement(session -> 
+		session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		httpSecurity.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return httpSecurity.build();
 	}
