19c745d81a77b3efeedeb734a1e2ab72b9626f4a
NM <masihnishit@gmail.com>
2023-01-24 00:10:51 +0530

    video 111

--- src/main/java/com/alpha/www/SpringBootBlogApp/entity/Post.java
+++ src/main/java/com/alpha/www/SpringBootBlogApp/entity/Post.java
@@ -6,9 +6,12 @@ import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -41,5 +44,9 @@ public class Post {
 	
 	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Set<Comment> comments = new HashSet();
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id") // foreign key column, child table
+	private Category category;
 	
 }
